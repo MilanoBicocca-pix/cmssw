e291c5f9e8878ade23022949bf69b261da4d0201
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2015-11-25 10:16:46 +0100

    up

--- RecoVertex/BeamSpotProducer/test/BeamFit_LumiBased_NewAlignWorkflow.py
+++ RecoVertex/BeamSpotProducer/test/BeamFit_LumiBased_NewAlignWorkflow.py
@@ -4,7 +4,8 @@ process = cms.Process("BSworkflow")
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-      "/store/data/Run2015A/ZeroBias/RECO/PromptReco-v1/000/248/038/00000/5C3E92F8-6614-E511-BF18-02163E0143EC.root",
+#       "/store/data/Run2015A/ZeroBias/RECO/PromptReco-v1/000/248/038/00000/5C3E92F8-6614-E511-BF18-02163E0143EC.root",
+      "/store/data/Run2015D/ZeroBias/RECO/PromptReco-v4/000/258/159/00000/0007AF4D-B36B-E511-8407-02163E012385.root",
     )
 )
 
@@ -26,27 +27,34 @@ process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
 process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff")
 process.load('Configuration.Geometry.GeometryRecoDB_cff')
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
-process.GlobalTag.globaltag = '74X_dataRun2_Candidate_2015_10_06_09_25_21'
+# process.GlobalTag.globaltag = '74X_dataRun2_Candidate_2015_10_06_09_25_21'
+process.GlobalTag.globaltag = '74X_dataRun2_Candidate_2015_11_18_10_38_33'
 
 ## Track refit
 process.load("RecoTracker.TrackProducer.TrackRefitters_cff")
 
 # remove the following lines if you run on RECO files
-process.TrackRefitter.src = 'ALCARECOTkAlMinBias'
-process.TrackRefitter.NavigationSchool = ''
+# process.TrackRefitter.src = 'ALCARECOTkAlMinBias'
+# process.TrackRefitter.NavigationSchool = ''
+
+
+# 
+process.load("RecoTracker.MeasurementDet.MeasurementTrackerEventProducer_cfi")
+process.MeasurementTrackerEvent.inactivePixelDetectorLabels = cms.VInputTag()
+process.MeasurementTrackerEvent.inactiveStripDetectorLabels = cms.VInputTag() 
 
 
 ## PV refit
 process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
 
 from RecoVertex.PrimaryVertexProducer.OfflinePrimaryVertices_cfi import offlinePrimaryVertices 
-process.offlinePrimaryVerticesFromRefittedTrks  = offlinePrimaryVertices.clone()
-process.offlinePrimaryVerticesFromRefittedTrks.TrackLabel                                       = cms.InputTag("TrackRefitter") 
-process.offlinePrimaryVerticesFromRefittedTrks.vertexCollections.maxDistanceToBeam              = 1
-process.offlinePrimaryVerticesFromRefittedTrks.TkFilterParameters.maxNormalizedChi2             = 20
-process.offlinePrimaryVerticesFromRefittedTrks.TkFilterParameters.minSiliconLayersWithHits      = 5
-process.offlinePrimaryVerticesFromRefittedTrks.TkFilterParameters.maxD0Significance             = 5.0 
-process.offlinePrimaryVerticesFromRefittedTrks.TkFilterParameters.minPixelLayersWithHits        = 2   
+process.offlinePrimaryVerticesFromRefittedTrks                                             = offlinePrimaryVertices.clone()
+process.offlinePrimaryVerticesFromRefittedTrks.TrackLabel                                  = cms.InputTag("TrackRefitter") 
+process.offlinePrimaryVerticesFromRefittedTrks.vertexCollections.maxDistanceToBeam         = 1
+process.offlinePrimaryVerticesFromRefittedTrks.TkFilterParameters.maxNormalizedChi2        = 20
+process.offlinePrimaryVerticesFromRefittedTrks.TkFilterParameters.minSiliconLayersWithHits = 5
+process.offlinePrimaryVerticesFromRefittedTrks.TkFilterParameters.maxD0Significance        = 5.0 
+process.offlinePrimaryVerticesFromRefittedTrks.TkFilterParameters.minPixelLayersWithHits   = 2   
 
 
                      
@@ -80,7 +88,9 @@ process.d0_phi_analyzer.BSAnalyzerParameters.resetEveryNLumi = 1
 
 
 process.p = cms.Path(
-                     process.offlineBeamSpot                        + 
-                     process.TrackRefitter                          + 
-                     process.offlinePrimaryVerticesFromRefittedTrks +
-                     process.d0_phi_analyzer)
+    process.offlineBeamSpot                        + 
+    process.MeasurementTrackerEvent                + # RIC 23/11/15, needed to fix missing product "Looking for type: MeasurementTrackerEvent"
+    process.TrackRefitter                          + 
+    process.offlinePrimaryVerticesFromRefittedTrks +
+    process.d0_phi_analyzer
+)
--- RecoVertex/BeamSpotProducer/test/crab_LumiBased_cfg.py
+++ RecoVertex/BeamSpotProducer/test/crab_LumiBased_cfg.py
@@ -3,17 +3,48 @@ from CRABClient.UserUtilities import config
 config = config()
 
 config.General.transferOutputs = True
-config.General.requestName     = 'Run2015A_6Oct15_74X_dataRun2_Candidate_2015_10_06_09_25_21'
-config.General.workArea        = 'bs_crab'
+
+# 2015A
+# config.General.requestName     = '2015A_23nov15_GT_74X_dataRun2_Candidate_2015_11_18_10_38_33_v2'
+# config.General.workArea        = 'crab_nov15_rereco_2015A_v2'
+# config.Data.inputDataset       = '/ZeroBias/Run2015A-PromptReco-v1/RECO'
+
+# 2015B
+# config.General.requestName     = '2015B_23nov15_GT_74X_dataRun2_Candidate_2015_11_18_10_38_33_v2'
+# config.General.workArea        = 'crab_nov15_rereco_2015B_v2'
+# config.Data.inputDataset       = '/ZeroBias/Run2015B-PromptReco-v1/RECO'
+
+# 2015C
+# config.General.requestName     = '2015C_23nov15_GT_74X_dataRun2_Candidate_2015_11_18_10_38_33_v2'
+# config.General.workArea        = 'crab_nov15_rereco_2015C_v2'
+# config.Data.inputDataset       = '/ZeroBias/Run2015C-PromptReco-v1/RECO'
+
+# 2015Dv3
+# config.General.requestName     = '2015D_v3_23nov15_GT_74X_dataRun2_Candidate_2015_11_18_10_38_33_v2'
+# config.General.workArea        = 'crab_nov15_rereco_2015D_v3_v2'
+# config.Data.inputDataset       = '/ZeroBias/Run2015D-PromptReco-v3/RECO'
+
+# 2015Dv4
+# config.General.requestName     = '2015D_v4_23nov15_GT_74X_dataRun2_Candidate_2015_11_18_10_38_33_v2'
+# config.General.workArea        = 'crab_nov15_rereco_2015D_v4_v2'
+# config.Data.inputDataset       = '/ZeroBias/Run2015D-PromptReco-v4/RECO'
+
+# 2015E
+config.General.requestName     = '2015E_23nov15_GT_74X_dataRun2_Candidate_2015_11_18_10_38_33_v2'
+config.General.workArea        = 'crab_nov15_rereco_2015E_v2'
+config.Data.inputDataset       = '/ZeroBias/Run2015E-PromptReco-v1/RECO'
+
 
 config.JobType.psetName        = 'BeamFit_LumiBased_NewAlignWorkflow.py'
 config.JobType.pluginName      = 'Analysis'
 config.JobType.outputFiles     = ['BeamFit_LumiBased_NewAlignWorkflow_alcareco.txt']
 
-config.Data.inputDataset       = '/ZeroBias/Run2015A-PromptReco-v1/RECO'
 config.Data.unitsPerJob        = 10
 config.Data.splitting          = 'LumiBased'
-config.Data.lumiMask           = 'total_DCS_json_upTo256869.txt'
+
+# JSON files:
+# /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/
+config.Data.lumiMask           = 'json_DCSONLY.txt'
 # config.Data.runRange           = '246908-247644'
 config.Data.publication        = False
 config.Data.outLFNDirBase      = '/store/group/phys_tracking/beamspot/'
