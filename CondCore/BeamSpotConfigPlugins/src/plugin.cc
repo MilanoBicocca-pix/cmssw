083a4a3c2a2dbea4c945b77ce04160e4d10105cc
Francesco Brivio <francesco.brivio@cern.ch>
2018-08-09 15:24:38 +0200

    start adding plugin part

---
+++ CondCore/BeamSpotConfigPlugins/src/plugin.cc
@@ -0,0 +1,5 @@
+#include "CondCore/PluginSystem/interface/registration_macros.h"
+#include "CondFormats/DataRecord/interface/BeamSpotThresholdsRcd.h"
+#include "CondFormats/BeamSpotConfig/interface/BeamSpotThresholds.h"
+
+REGISTER_PLUGIN(BeamSpotThresholdsRcd,BeamSpotThresholds);
